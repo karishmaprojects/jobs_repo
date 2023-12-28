84071f16b7111f4e72393d8857a957d6670b18ed
karishmaprojects <153348228+karishmaprojects@users.noreply.github.com>
2023-12-28 12:30:42 +0530

    Update settings.py

--- jobsproject1/jobsproject1/settings.py
+++ jobsproject1/jobsproject1/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'jobsproject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jobsdb',
-        'USER':'root',
-        'PASSWORD':'root',
-        'HOST':'localhost',
-        'PORT':3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
